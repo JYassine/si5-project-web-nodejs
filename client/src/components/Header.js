9f5a71e08bf3ab7e8451b4482c303ffa6f3f3597
Adrien <adrien.vasseur@etu.unice.fr>
2021-02-15 15:49:32 +0100

    #7 Added navbar

---
+++ client/src/components/Header.js
@@ -0,0 +1,10 @@
+import './Header.css';
+
+export const Header = () => {
+    return (
+        <header className="header-bar">
+            <p className="app-title">COVID 19 INFO</p>
+            <p className="theme-button">Night Mode Here</p>
+        </header>
+    )
+};
